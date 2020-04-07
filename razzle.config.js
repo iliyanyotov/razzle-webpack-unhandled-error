da3c520d8e428a263390a614b8afca5e3be3ff67
Iliyan Yotov <iliyan.yotov@gmail.com>
2020-04-07 14:23:00 +0300

    fix: Updating the readme and adding a screenshot

--- razzle.config.js
+++ razzle.config.js
@@ -4,7 +4,7 @@ module.exports = {
   modify: (config, { dev, target }) => {
     /**
      * Example customization by pushing BundleAnalyzerPlugin to the plugins array
-     * Take into consideration that the package is not installed on purpose
+     * Take into consideration the package is not installed on purpose
      */
     if (target === 'web' && process.env.BUNDLE_ANALYZE) {
       config.plugins = [
